077dd27f498ef15157d870505863f065a86205f7
Chris Mullins <cmullins@liveramp.com>
2017-01-18 22:17:30 -0800

    REST-ish routes for getting and posting IR commands

--- src/main.cpp
+++ src/main.cpp
@@ -19,21 +19,6 @@ void setup() {
   Serial.begin(115200);
   wifiManager.autoConnect();
   
-  server.on("/", HTTP_GET, [](){
-    String body = "<html><head><title>IR Server</title></head>";
-    body += "<body>";
-    body += "<form method='post' action='/ir'>";
-    body += "<h5>Type</h5>";
-    body += "<div><input type='radio' name='type' value='sony' /> Sony</div>";
-    body += "<div><input type='radio' name='type' value='raw' /> Raw</div>";
-    body += "<h5>Code</h5>";
-    body += "<textarea rows='3' cols='80' style='font-family: courier new, courier, mono;'></textarea>";
-    body += "<input type='submit' />";
-    body += "</form></body></html>";
-    
-    server.send(200, "text/html", body);
-  });
-  
   server.on("/ir", HTTP_POST, []() {
     StaticJsonBuffer<1024> buffer;
     JsonObject& request = buffer.parse(server.arg("plain"));
@@ -48,7 +33,7 @@ void setup() {
           yield();
         }
     
-        server.send(200, "text/plain", server.arg("plain"));
+        server.send(200, "text/plain", "true");
       } else if (request["type"] == "raw") {
         JsonArray& data = request["data"];
         unsigned int * buffer = new unsigned int[data.size()];
@@ -57,16 +42,7 @@ void setup() {
         
         irsend.sendRaw(buffer, data.size(), pwmFrequency);
         
-        String response = "With data: [";
-        for (int i = 0; i < data.size(); i++) {
-          response += buffer[i];
-          response += ",";
-        }
-        response += "]\n";
-        response += "size: " + String(data.size()) + "\n";
-        response += "freq: " + String(pwmFrequency);
-        
-        server.send(200, "text/plain", response);
+        server.send(200, "text/plain", "true");
       }
     }
   });
